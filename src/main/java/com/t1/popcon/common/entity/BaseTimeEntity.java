f184c7b3118380144ad01f455da478c7808a7d83
son_mac <smk2591@naver.com>
2026-02-07 21:13:52 +0900

    feat: BaseEntity 계층 구조 구현 (Id, Time, Audit, SoftDelete) (#2)

---
+++ src/main/java/com/t1/popcon/common/entity/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.t1.popcon.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity extends BaseIdEntity {
+	@CreatedDate
+	protected LocalDateTime createdAt;
+
+	@LastModifiedDate
+	protected LocalDateTime updatedAt;
+}
