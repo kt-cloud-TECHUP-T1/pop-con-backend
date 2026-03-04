a59150cec88acef1d390ede8f0478c178724098c
hyeonda02 <kdh021022@gmail.com>
2026-03-04 15:17:41 +0900

    chore: 서비스 환경별 application-*.yml 파일 추가

--- user-service/src/main/java/com/t1/popcon/user/config/SwaggerConfig.java
+++ user-service/src/main/java/com/t1/popcon/user/config/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package com.t1.popcon.user.config;
 
-public class SwaggerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile({"default", "local", "prod"})
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String projectPath = System.getProperty("user.dir");
+
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations(
+                        "file:" + projectPath + "/build/api-spec/",
+                        "classpath:/static/swagger-ui/"
+                );
+
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/static/");
+    }
 }
