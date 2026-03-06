2a09d89553572c0d65e657f3272a8459df10fac4
youngyii <yishinyoung0708@gmail.com>
2026-03-06 19:30:15 +0900

    feat: 팝업 섹션/카드 응답 DTO 추가

---
+++ popup-service/src/main/java/com/t1/popcon/popup/dto/card/OverlayType.java
@@ -0,0 +1,7 @@
+package com.t1.popcon.popup.dto.card;
+
+public enum OverlayType {
+    RANK,
+    AUCTION_OPEN_AT,
+    DRAW_OPEN_AT
+}
---
+++ popup-service/src/main/java/com/t1/popcon/popup/dto/section/SectionKey.java
@@ -0,0 +1,14 @@
+package com.t1.popcon.popup.dto.section;
+
+public enum SectionKey {
+    // Home
+    BANNERS,
+    RECOMMENDED,
+    RANKINGS_WEEKLY,
+    AUCTIONS,
+    DRAWS_OPEN,
+    DRAWS_UPCOMING,
+    FEATURED,
+    ENDING_SOON,
+    MAGAZINES
+}
