5bc901f00d62396336f2819204fe9a0fb2211698
Shinyoung <104247324+youngyii@users.noreply.github.com>
2026-03-17 10:32:45 +0900

    feat: 드로우 날짜·회차별 관리 엔티티 및 조회 API 구현 (#79)
    * feat: 드로우 에러코드 추가
    
    * feat: DrawOption 엔티티 추가
    
    * feat: 드로우 회차 목록 DTO 추가
    
    * feat: DrawOptionRepository 추가
    
    * feat: DrawOption 서비스, 컨트롤러 추가
    
    - feat: DrawOption 서비스, 컨트롤러 추가
    - refactor: draw 엔티티 soft delete 필터 추가 및 repository 정리
    
    * fix: active row만 유니크하도록 수정 및 오타 수정
    
    * feat: 회차별 응모 가능 수량 검증 추가

--- draw-service/src/main/java/com/t1/popcon/draw/domain/Draw.java
+++ draw-service/src/main/java/com/t1/popcon/draw/domain/Draw.java
@@ -8,13 +8,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "draws")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "draws")
+@SQLRestriction("deleted = false")
 public class Draw extends BaseSoftDeleteEntity {
 
     @Column(nullable = false, unique = true)
@@ -27,19 +29,20 @@ public class Draw extends BaseSoftDeleteEntity {
     private LocalDateTime drawCloseAt;
 
     @Column(nullable = false)
-    private Integer maxDrawQuantityPerRound;
+    private Integer stockPerOption;
 
     @Builder
     public Draw(
             Long popupId,
             LocalDateTime drawOpenAt,
             LocalDateTime drawCloseAt,
-            Integer maxDrawQuantityPerRound
+            Integer stockPerOption
     ) {
+        validateStockPerOption(stockPerOption);
         this.popupId = popupId;
         this.drawOpenAt = drawOpenAt;
         this.drawCloseAt = drawCloseAt;
-        this.maxDrawQuantityPerRound = maxDrawQuantityPerRound;
+        this.stockPerOption = stockPerOption;
     }
 
     public void updateSchedule(LocalDateTime drawOpenAt, LocalDateTime drawCloseAt) {
@@ -47,7 +50,14 @@ public class Draw extends BaseSoftDeleteEntity {
         this.drawCloseAt = drawCloseAt;
     }
 
-    public void updateMaxDrawQuantityPerRound(Integer maxDrawQuantityPerRound) {
-        this.maxDrawQuantityPerRound = maxDrawQuantityPerRound;
+    public void updateStockPerOption(Integer stockPerOption) {
+        validateStockPerOption(stockPerOption);
+        this.stockPerOption = stockPerOption;
+    }
+
+    private void validateStockPerOption(Integer stockPerOption) {
+        if (stockPerOption == null ||  stockPerOption <= 0) {
+            throw new IllegalArgumentException("회차당 당첨 수량은 0보다 커야 합니다.");
+        }
     }
 }
