22544078719727dd3781028f5f0c8ba4eb84d615
son_mac <smk2591@naver.com>
2026-03-26 16:15:41 +0900

    feat: @EnableJpaAuditing 중복 설정 제거, common모듈 Java 버전 명시

--- auth-service/src/main/java/com/t1/popcon/AuthApplication.java
+++ auth-service/src/main/java/com/t1/popcon/AuthApplication.java
@@ -2,11 +2,9 @@ package com.t1.popcon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableJpaAuditing
 @SpringBootApplication
 public class AuthApplication {
 	public static void main(String[] args) {
