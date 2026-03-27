0b15aa69acea36db1a88a068513b29f7975b8f23
Son Moo Kyung <88808478+son2304@users.noreply.github.com>
2026-03-27 17:33:53 +0900

    refactor: 포트원 빌링키 연동 고도화 진행 + 결제 관련 수정 + 빌링키 조회/삭제 (#114)
    * refactor: 포트원 빌링키 연동 고도화 진행 (빌링키 등록, 결제 요청, 결제 취소 요청) + 빌링키 조회/삭제 + 대표 결제수단 설정 로직 추가
    
    * refactor: 코드래빗 수정사항 적용
    
    * refactor: 빌링키 - 유저 매핑
    
    * refactor: CORS 에러 수정
    
    * refactor: 이미 대표결제 수단인 케이스 추가
    
    * refactor: 코드래빗 수정사항 적용
    
    * refactor: 코드래빗 수정사항 적용

---
+++ auction-service/src/main/java/com/t1/popcon/auction/bid/client/UserBillingClient.java
@@ -0,0 +1,20 @@
+package com.t1.popcon.auction.bid.client;
+
+import com.t1.popcon.auction.bid.client.config.FeignClientConfig;
+import com.t1.popcon.auction.bid.client.dto.BillingKeyInternalResponse;
+import com.t1.popcon.common.response.ApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+  name = "user-service",
+  url = "${services.user-service.url:http://localhost:8081}",
+  configuration =  FeignClientConfig.class
+
+)
+public interface UserBillingClient {
+
+    @GetMapping("/internal/billing/keys/default")
+    ApiResponse<BillingKeyInternalResponse> getDefaultBillingKey(@RequestParam("userId") Long userId);
+}
---
+++ user-service/src/main/java/com/t1/popcon/user/billing/dto/BillingKeyInternalResponse.java
@@ -0,0 +1,5 @@
+package com.t1.popcon.user.billing.dto;
+
+public record BillingKeyInternalResponse(
+    String customerUid
+) {}
