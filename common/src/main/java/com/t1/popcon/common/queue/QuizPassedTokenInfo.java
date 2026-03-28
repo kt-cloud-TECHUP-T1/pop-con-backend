55669594e767b3e80dc81240852c5ba6f4084ef5
youngyii <yishinyoung0708@gmail.com>
2026-03-28 22:11:12 +0900

    feat: queue-common 모듈 추가, 도메인 enum 추가

--- common/src/main/java/com/t1/popcon/common/queue/QuizPassedTokenInfo.java
+++ common/src/main/java/com/t1/popcon/common/queue/QuizPassedTokenInfo.java
@@ -4,7 +4,7 @@ package com.t1.popcon.common.queue;
  * 퀴즈 통과 토큰에서 추출한 사용자 식별 정보
  */
 public record QuizPassedTokenInfo(
-	String phaseType,  // DRAW / AUCTION
+	String phaseType,  // draw / auction
 	Long phaseId,      // drawId / auctionId
 	Long userId
 ) {
