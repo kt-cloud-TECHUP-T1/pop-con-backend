bdd35077c996c0a0d4b4ac3cf64625ee9cce8a59
Mingi Kim <zxc12250@naver.com>
2026-03-30 23:33:47 +0900

    feat: 티켓 모듈 생성 및 환경설정

---
+++ ticket-service/src/main/java/com/t1/popcon/TicketApplication.java
@@ -0,0 +1,14 @@
+package com.t1.popcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class TicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketApplication.class, args);
+    }
+}
