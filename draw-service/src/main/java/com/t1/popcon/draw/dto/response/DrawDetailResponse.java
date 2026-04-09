9509061342c461d673433ef86cc8d84316e1bf58
Shinyoung <104247324+youngyii@users.noreply.github.com>
2026-04-09 11:29:29 +0900

    fix: DrawDetailResponse serverTime 밀리초 제외 (#181)
    * fix: DrawDetailResponse serverTime 밀리초 제외
    
    * chore: application 설정 debug->info로 변경

--- draw-service/src/main/java/com/t1/popcon/draw/dto/response/DrawDetailResponse.java
+++ draw-service/src/main/java/com/t1/popcon/draw/dto/response/DrawDetailResponse.java
@@ -1,6 +1,5 @@
 package com.t1.popcon.draw.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 public record DrawDetailResponse(
@@ -8,7 +7,6 @@ public record DrawDetailResponse(
         LocalDateTime drawOpenAt,
         LocalDateTime drawCloseAt,
         boolean participatable,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") // 밀리초 제외, 초까지만 반환
         LocalDateTime serverTime
 ) {
     public static DrawDetailResponse of(
